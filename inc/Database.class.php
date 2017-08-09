95f59d8e525519b5925960d7150e9aa74ad5b8c7
ben993 <tn00371361@gmail.com>
2017-08-09 14:00:10 +0800

    phpajax

---
+++ inc/Database.class.php
@@ -0,0 +1,29 @@
+<?php
+class Database
+{
+    private static $dbName = '0809';
+    private static $dbHost = 'localhost';
+    private static $dbUsername = 'test123';
+    private static $dbUserPassword = 'test123';
+
+    private static $cont = null;
+
+    public function __construct() {
+        die('Init function is not allowed');
+    }
+
+    public static function connect() {
+        if (null === self::$cont) {
+            try {
+                self::$cont =  new PDO('mysql:host='.self::$dbHost.'; dbname='.self::$dbName, self::$dbUsername, self::$dbUserPassword);
+            } catch(PDOException $e) {
+                die($e->getMessage());
+            }
+        }
+        return self::$cont;
+    }
+
+    public static function disconnect() {
+        self::$cont = null;
+    }
+}
